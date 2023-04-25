30991b9c752213ec8e0d28f2f5ee35b3de46f284
ImranHossain1 <imranhossain1402@gmail.com>
2023-04-26 01:58:59 +0200

    add a reset button to reset the cookies

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,34 +4,68 @@ import Cookies from "js-cookie";
 import categories from "../../utils/categories.json";
 import Carousel from "react-material-ui-carousel";
 import Item from "./Item";
+import { Button } from "@mui/material";
+
 const Home = () => {
   const Category = Cookies.get("Category");
   const categoryData = categories.filter(
     (category) => category.category === Category
   );
+
+  const handleReset = () => {
+    Cookies.remove("Category");
+    window.location.reload(); // Reload the page to reflect the change
+  };
+
   return (
     <div>
       {Category ? (
-        <Carousel
-          indicators={true}
-          indicatorIconButtonProps={{
-            style: { backgroundColor: "white" },
-          }}
-          indicatorContainerProps={{
-            style: {
+        <div style={{ position: "relative", flex: 1 }}>
+          <Carousel
+            indicators={true}
+            indicatorIconButtonProps={{
+              style: { backgroundColor: "white" },
+            }}
+            indicatorContainerProps={{
+              style: {
+                position: "absolute",
+                bottom: 20,
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                gap: "10px",
+              },
+            }}
+          >
+            {categoryData.map((category) => (
+              <Item key={category._id} category={category} />
+            ))}
+          </Carousel>
+          <div
+            style={{
               position: "absolute",
-              bottom: 20,
+              bottom: -40,
               width: "100%",
               display: "flex",
               justifyContent: "center",
-              gap: "10px",
-            },
-          }}
-        >
-          {categoryData.map((category) => (
-            <Item key={category._id} category={category} />
-          ))}
-        </Carousel>
+            }}
+          >
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleReset}
+              style={{
+                width: "40%",
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Reset your choice
+            </Button>
+          </div>
+        </div>
       ) : (
         <StartScreen></StartScreen>
       )}
